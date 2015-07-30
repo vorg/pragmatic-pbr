216fce60fc0b8224f1d665477a0c7532697fe783
Marcin Ignac <marcin.ignac@gmail.com>
2015-07-30 09:51:43 +0100

    201-init added

---
+++ 201-init/main.js
@@ -0,0 +1,22 @@
+var Window = require('pex-sys/Window');
+
+var frame = 0;
+
+Window.create({
+    settings: {
+        width: 1024,
+        height: 576
+    },
+    init: function() {
+        console.log('init');
+    },
+    draw: function() {
+        var ctx = this.getContext();
+        frame++;
+        var r = 0.5 + 0.5 * Math.sin(frame/10);
+        var g = 0.5 + 0.5 * Math.cos(frame/10 + Math.PI/2);
+        var b = 0.5 + 0.5 * Math.sin(frame/10 + Math.PI/4);
+        ctx.setClearColor(r, g, b, 1.0);
+        ctx.clear(ctx.COLOR_BIT);
+    }
+})
