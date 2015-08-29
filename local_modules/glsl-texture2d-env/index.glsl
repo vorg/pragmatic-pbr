bdbc3644781847eaa8da9f738d5f0f0eb8822055
Marcin Ignac <marcin.ignac@gmail.com>
2015-08-29 21:58:02 +0100

    glsl-texture2d-env renamed to glsl-texture2d-latlong

--- local_modules/glsl-texture2d-env/index.glsl
+++
@@ -1,6 +0,0 @@
-vec4 texture2DEnv(sampler2D tex, vec3 N) {
-    vec2 texCoord = vec2((1.0 + atan(-N.z, N.x)/3.14159265359)/2.0, acos(N.y)/3.14159265359);
-    return texture2D(tex, texCoord);
-}
-
-#pragma glslify: export(texture2DEnv)
---
+++ local_modules/glsl-texture2d-latlong/index.glsl
@@ -0,0 +1,14 @@
+#define PI 3.1415926
+#define TwoPI (2.0 * PI)
+#define OneOverPI (1.0/PI)
+#define OneOverTwoPI (1.0/TwoPI)
+
+//Based on http://http.developer.nvidia.com/GPUGems/gpugems_ch17.html
+vec4 texture2DLatLong(sampler2D tex, vec3 N) {
+    float theta = acos(N.y);
+    float phi = atan(N.x, N.z) + PI;
+    vec2 texCoord = vec2(phi, theta) * vec2(OneOverTwoPI, OneOverPI);
+    return texture2D(tex, texCoord);
+}
+
+#pragma glslify: export(texture2DLatLong)
