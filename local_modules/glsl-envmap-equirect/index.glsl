d02b4470e624801f4a073d5f73b44687fc462ef4
Marcin Ignac <marcin.ignac@gmail.com>
2015-10-06 23:24:03 +0100

    glsl-texture2d-env-latlong renamed to glsl-envmap-equirect

--- local_modules/glsl-envmap-equirect/index.glsl
+++ local_modules/glsl-envmap-equirect/index.glsl
@@ -7,14 +7,20 @@
  * @param  {vec3} wcNormal - normal in the world coordinate space
  * @param  {float} - flipEnvMap    -1.0 for left handed coorinate system oriented texture (usual case)
  *                                  1.0 for right handed coorinate system oriented texture
- * @return {vec4} - sampledColor
+ * @return {vec2} equirectangular texture coordinate-
  * @description Based on http://http.developer.nvidia.com/GPUGems/gpugems_ch17.html and http://gl.ict.usc.edu/Data/HighResProbes/
  */
-vec4 texture2DEnvLatLong(sampler2D envMap, vec3 wcNormal, float flipEnvMap) {
-    float phi = acos(wcNormal.y);
-    float theta = atan(flipEnvMap * wcNormal.x, wcNormal.z) + PI;
-    vec2 texCoord = vec2(theta / TwoPI, phi / PI);
-    return texture2D(envMap, texCoord);
+vec2 envMapEquirect(vec3 wcNormal, float flipEnvMap) {
+  //I assume envMap texture has been flipped the WebGL way (pixel 0,0 is a the bottom)
+  //therefore we flip wcNorma.y as acos(1) = 0
+  float phi = acos(-wcNormal.y);
+  float theta = atan(flipEnvMap * wcNormal.x, wcNormal.z) + PI;
+  return vec2(theta / TwoPI, phi / PI);
 }
 
-#pragma glslify: export(texture2DEnvLatLong)
+vec2 envMapEquirect(vec3 wcNormal) {
+    //-1.0 for left handed coorinate system oriented texture (usual case)
+    return envMapEquirect(wcNormal, -1.0);
+}
+
+#pragma glslify: export(envMapEquirect)
