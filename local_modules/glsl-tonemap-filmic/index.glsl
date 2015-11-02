542b77caff755d08e605c02f9c64c921b974b414
Marcin Ignac <marcin.ignac@gmail.com>
2015-11-02 00:49:06 +0000

    glsl-tonemap-filmic initial

--- local_modules/glsl-tonemap-filmic/index.glsl
+++ local_modules/glsl-tonemap-filmic/index.glsl
@@ -1,6 +1,7 @@
 //Based on Filmic Tonemapping Operators http://filmicgames.com/archives/75
 vec3 tonemapFilmic(vec3 color) {
-  return vec3(1.0, 0.0, 0.0);
+    vec3 x = max(vec3(0.0), color - 0.004);
+    return (x * (6.2 * x + 0.5)) / (x * (6.2 * x + 1.7) + 0.06);
 }
 
 #pragma glslify: export(tonemapFilmic)
