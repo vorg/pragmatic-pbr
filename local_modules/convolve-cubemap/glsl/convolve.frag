3ae9722c7f6e00d2a67d38525f056f59b6c7f1ec
Marcin Ignac <marcin.ignac@gmail.com>
2016-04-02 00:33:56 +0100

    convolve-cubemap new implementation

---
+++ local_modules/convolve-cubemap/glsl/convolve.frag
@@ -0,0 +1,34 @@
+//Based on Article - Physically Based Rendering by Marco Alamia
+//http://www.codinglabs.net/article_physically_based_rendering.aspx
+
+#ifdef GL_ES
+precision highp float;
+#endif
+
+varying vec3 wcNormal;
+varying vec2 scPosition;
+
+uniform samplerCube uEnvMap;
+uniform float uFace;
+
+const float PI = 3.1415926536;
+
+void main() {
+    vec3 normal = normalize( wcNormal );
+    vec3 up = vec3(0,1,0);
+    vec3 right = normalize(cross(up,normal));
+    up = cross(normal,right);
+
+    vec3 sampledColour = vec3(0,0,0);
+    float index = 0;
+    for(float phi = 0; phi < 6.283; phi += 0.0345) { //180 samples
+        for(float theta = 0; theta < 1.57; theta += 0.0245) { //64 samples
+            vec3 temp = cos(phi) * right + sin(phi) * up;
+            vec3 sampleVector = cos(theta) * normal + sin(theta) * temp;
+            sampledColour += textureCube( uEnvMap, sampleVector ).rgb * cos(theta) * sin(theta);
+            index++;
+        }
+    }
+
+    gl_FragColor = vec4(PI * sampledColour / index, 1.0);
+}
---
+++ local_modules/convolve-cubemap/index.js
@@ -0,0 +1,33 @@
+var renderToCubemap = require('../render-to-cubemap');
+var glslifySync = require('glslify-sync');
+var hammersley = require('hammersley');
+
+var VERT = glslifySync(__dirname + '/glsl/convolve.vert');
+var FRAG = glslifySync(__dirname + '/glsl/convolve.frag');
+
+var quadPositions = [[-1,-1],[1,-1], [1,1],[-1,1]];
+var quadFaces = [ [0, 1, 2], [0, 2, 3]];
+
+var quadMesh = null;
+var convolveProgram = null;
+
+function convolveCubemap(ctx, fromCubemap, toCubemap) {
+    ctx.pushState(ctx.MESH_BIT | ctx.PROGRAM_BIT); //ctx.TEXTURE_BIT
+    if (!quadMesh) {
+        var quadAttributes = [ { data: quadPositions, location: ctx.ATTRIB_POSITION } ];
+        var quadIndices = { data: quadFaces };
+        quadMesh = ctx.createMesh(quadAttributes, quadIndices);
+
+        convolveProgram = ctx.createProgram(VERT, FRAG);
+    }
+    renderToCubemap(ctx, toCubemap, function() {
+        ctx.bindTexture(fromCubemap, 0);
+        ctx.bindProgram(convolveProgram);
+        convolveProgram.setUniform('uEnvMap', 0);
+        ctx.bindMesh(quadMesh);
+        ctx.drawMesh();
+    });
+    ctx.popState(ctx.MESH_BIT | ctx.PROGRAM_BIT); //ctx.TEXTURE_BIT
+}
+
+module.exports = convolveCubemap;
